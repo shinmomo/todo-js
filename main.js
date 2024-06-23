bf700725f5063b29ea0c8ad4d17b202b3134e3b3
shinmomo <shinmomo19450902@gmail.com>
2024-06-23 22:55:32 +0900

    javascript一部

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value ="";
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
